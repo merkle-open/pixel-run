d18934b71d7af7669d6a786fc150b3a061ba5ab2
Jan Biasi <jan.biasi@namics.com>
2016-01-07 11:36:00 +0100

    Flush working state of modular branch

--- app/game.js
+++ app/game.js
@@ -1,18 +1,4 @@
 (function(window, undefined) {
     'use strict';
 
-    App.register('Game', {
-        dependencies: ['Media', 'World', 'Render']
-    }, function(module, settings) {
-        module.publish('start', function() {
-            var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-               preload: App.modules.Media.access('init'),
-               create: App.modules.World.access('init'),
-               update: App.modules.Render.access('init')
-           });
-
-           module.publish('game', game);
-       });
-    });
-
 })(window);
--- app/media.js
+++ app/media.js
@@ -1,16 +1,4 @@
 (function(window, undefined) {
     'use strict';
 
-    App.register('Media', {
-        dependencies: []
-    }, function(module) {
-        module.publish('init', function() {
-            var game = App.modules.Game.access('game');
-            game.load.image('sky', 'assets/img/sky.png');
-            game.load.image('ground', 'assets/img/platform.png');
-            game.load.image('star', 'assets/img/star.png');
-            game.load.spritesheet('dude', 'assets/img/dude.png', 32, 48);
-        });
-    });
-
 })(window);
