295129ee0c66b1d0d92402e2164d70f94209fb14
Jan Biasi <jan.biasi@namics.com>
2016-02-01 17:36:20 +0100

    Fix buildprocess failure

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,19 @@ var header = require('gulp-header');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var commentify = require('gulp-header');
 var config = require('./build.json');
+var pkg = require('./package.json');
+
+var banner = commentify(['/**',
+  ' * <%= pkg.header %> ',
+  ' * @author <%= pkg.author %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %> Licensed by <%= pkg.company %>',
+  ' */',
+  ''].join('\n'), { pkg: pkg });
 
 gulp.task('clean:dist', function() {
     return remove(config.clean.dist);
@@ -18,8 +30,12 @@ gulp.task('clean:dist:app', function() {
 });
 
 gulp.task('clean:dist:bundle', function() {
-    return remove(config.bundle.target + '/' + config.bundle.name + '.*');
-})
+    return remove(config.bundle.target + '/' + config.bundle.name + '*.js');
+});
+
+gulp.task('clean:dist:styles', function() {
+    return remove(config.styles.target + '/' + config.styles.name + '*.css');
+});
 
 gulp.task('clean:dist:dependencies', function() {
     return remove(config.dependencies.target + '/' + config.dependencies.name + '.*');
@@ -35,6 +51,7 @@ gulp.task('build:app', ['clean:dist:app'], function() {
     return gulp.src(config.app.files)
         .pipe(gulpif(config.app.minify === true, uglify()))
         .pipe(concat(config.app.name + '.js'))
+        //.pipe(banner)
         .pipe(gulp.dest(config.app.target));
 });
 
@@ -42,7 +59,15 @@ gulp.task('build:bundle', ['clean:dist:bundle', 'build:app', 'build:dependencies
     return gulp.src(config.app.target + '/*.js')
         .pipe(uglify())
         .pipe(concat(config.bundle.name + '.min.js'))
-        .pipe(gulp.dest(config.app.target));
+        //.pipe(banner)
+        .pipe(gulp.dest(config.bundle.target));
+});
+
+gulp.task('build:styles', ['clean:dist:styles'], function() {
+    return gulp.src(config.styles.files)
+        .pipe(cssmin())
+        .pipe(concat(config.styles.name + '.min.css'))
+        .pipe(gulp.dest(config.styles.target));
 });
 
 gulp.task('watch:app', function() {
@@ -59,11 +84,20 @@ gulp.task('watch:dependencies', function() {
     });
 });
 
+gulp.task('watch:styles', function() {
+    var watcher = gulp.watch(config.styles.files, ['build:styles']);
+    watcher.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
 gulp.task('bundle', ['build:bundle']);
 
 gulp.task('default', [
+    'build:styles',
     'build:dependencies',
     'build:app',
+    'watch:styles',
     'watch:dependencies',
     'watch:app'
 ]);
