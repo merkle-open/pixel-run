a8af3790e2b91675e4677a6b0685b93c567f2cdb
Jan Biasi <jan.biasi@namics.com>
2016-02-01 11:10:27 +0100

    Adding new default helper

---
+++ app/util/default.js
@@ -0,0 +1,29 @@
+(function(window, undefined) {
+    'use strict';
+    
+    Util.noop = function() {
+        // Basic no-operation method
+    };
+
+    /**
+     * Set the defaults for a variable. Custom handler can
+     * also be used to check the value (to prevent long conditions)
+     * @param  {T} input                Input value
+     * @param  {T} defaultValue         Default value
+     * @param  {Function} custom        Custom validator
+     * @return {T}                      Right value
+     */
+    Util.default = function(input, defaultValue, custom) {
+        if(typeof custom === 'function') {
+            if(custom(input) === true) {
+                return input;
+            } else {
+                return defaultValue;
+            }
+        } else if(!input || input === undefined || input === null) {
+            return defaultValue;
+        } else {
+            return input;
+        }
+    }
+})(window);
