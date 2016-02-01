2970c1f874837bb524870626315519dfe6480ceb
Jan Biasi <jan.biasi@namics.com>
2016-02-01 11:11:17 +0100

    Update exports

--- dist/application.js
+++ dist/application.js
@@ -1,6 +1,11 @@
 (function(window, undefined) {
     'use strict';
 
+    /**
+     * Generates a clone of an object (without proto values)
+     * @param  {Object} obj         Object to clone
+     * @return {Object}             Cloned object
+     */
     Util.clone = function(obj) {
         if (!obj || typeof obj !== 'object') {
             return obj;
@@ -18,7 +23,42 @@
 
 (function(window, undefined) {
     'use strict';
+    
+    Util.noop = function() {
+        // Basic no-operation method
+    };
 
+    /**
+     * Set the defaults for a variable. Custom handler can
+     * also be used to check the value (to prevent long conditions)
+     * @param  {T} input                Input value
+     * @param  {T} defaultValue         Default value
+     * @param  {Function} custom        Custom validator
+     * @return {T}                      Right value
+     */
+    Util.default = function(input, defaultValue, custom) {
+        if(typeof custom === 'function') {
+            if(custom(input) === true) {
+                return input;
+            } else {
+                return defaultValue;
+            }
+        } else if(!input || input === undefined || input === null) {
+            return defaultValue;
+        } else {
+            return input;
+        }
+    }
+})(window);
+
+(function(window, undefined) {
+    'use strict';
+
+    /**
+     * Hyphenates a camelCased string
+     * @param  {String} str         Any camel/pascal cased string
+     * @return {String}             Hyphenated string
+     */
     Util.hyphenate = function(str) {
         return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
     };
@@ -28,6 +68,11 @@
 (function(window, undefined) {
     'use strict';
 
+    /**
+     * Creates a new replacer instance
+     * @param {String} input        Template string
+     * @param {Object} data         Dataset
+     */
     function Replacer(input, data) {
         this.input = input;
         this.data = data;
@@ -35,6 +80,13 @@
     }
 
     Replacer.prototype = {
+        /**
+         * Main replace method, replaces each key set with {<value>} with
+         * the key associated in the data. Data can be passed in this method
+         * or directly in the constructor of the replacer.
+         * @param  {Object} data    Optional data
+         * @return {String}         Compiled string
+         */
         replace: function(data) {
             var result = this.input;
             if(data === undefined) {
@@ -48,50 +100,18 @@
         }
     };
 
-    Replacer.create = function(input, data) {
-        return new Replacer(input, data);
-    };
-
-    Util.Replacer = Replacer;
+    /**
+     * Shorthand for creation and calling the replace method,
+     * easiest usage for the replace logic.
+     * @param  {String} input       Template string
+     * @param  {Object} data        Dataset
+     * @return {String}             Compiled string
+     */
     Util.replace = function(input, data) {
         return new Replacer(input).replace(data);
     };
 
-})(window);
-
-(function(window, undefined) {
-    'use strict';
-
-    var root = window.Container;
-
-    function LoaderHandler(game, type, name, path) {
-        this.type = type;
-        this.name = name;
-        this.path = path;
-
-        return this.$generateHandler();
-    };
-
-    LoaderHandler.prototype = {
-        image: function(game, name, path) {
-            return function() {
-                game.load.image(Util.hyphenate(key), path);
-            }
-        },
-        sprite: function(game, name, path, x, y) {
-            return function() {
-                game.load.spritesheet(name, path, x, y);
-            }
-        },
-        tilemap: function(game, name, path) {
-            return function() {
-                return new window.Factory.Tilemap(game, name, path);
-            }
-        }
-    };
-
-
-    window.Factory.LoaderHandler = LoaderHandler;
+    Util.Replacer = Replacer;
 
 })(window);
 
@@ -100,40 +120,14 @@
 
     var root = window.Container;
 
-    function MapGenerator(options) {
-        this.tilemap = options.tilemap;
-        this.settings = options.gameSettings;
-        this.factory = {};
-        return this;
-    }
-
-    MapGenerator.protoytpe = {
-        updater: function(handler) {
-            return function() {
-                handler(root, window);
-            }
-        },
-        $createTilemap: function(game) {
-            var map = new Factory.Tilemap(game, this.tilemap.name);
-            map.addToGame(game);
-            map.addImage(this.tilemap.tile.name, this.tilemap.tile.id);
-            map.createLayer(this.tilemap.layer.name); // Works until here
-            map.resize(this.tilemap.layer.name);
-        },
-        apply: function(handler) {
-            return handler(this);
-        }
-    };
-
-    window.Factory.Map = MapGenerator;
-
-})(window);
-
-(function(window, undefined) {
-    'use strict';
-
-    var root = window.Container;
-
+    /**
+     * Creates a new player instance
+     * @param {Game} game           Reference to the game
+     * @param {Number} index        Index of the player (count)
+     * @param {Number} posX         Position on x-axis
+     * @param {Number} posY         Position on y-axis
+     * @param {String} variation    Special variation of player skin
+     */
     function Player(game, index, posX, posY, variation) {
         this.$baseSprite = root.settings.game.players.baseName;
         this.$basePath = root.settings.paths.player;
@@ -153,16 +147,29 @@
     Player.prototype = Object.create(Phaser.Sprite.prototype);
     Player.prototype.constructor = Player;
 
+    /**
+     * Generates the path to the right spritesheet of each
+     * player and its variation
+     * @return {String}         Path to the asset
+     */
     Player.prototype.$getSpritesheet = function() {
         this.type = this.type === undefined ? '-' + root.settings.game.players.variations[index] : '';
         return this.$baseSprite + this.type;
     };
 
+    /**
+     * Generates the action key settings and creates the right
+     * cursor for each player instance.
+     * @return {Key}            Phaser key stack
+     */
     Player.prototype.$addActionKey = function() {
         var cursors = this.injector.input.keyboard.createCursorKeys();
         return this.$actionKey = cursors[this.jumpKey];
     };
 
+    /**
+     * General player update method, containing the jump logic
+     */
     Player.prototype.$update = function() {
         var listenTo = this.$addActionKey();
         if(listenTo.isDown) {
@@ -170,21 +177,36 @@
         }
     };
 
+    /**
+     * Initializes a player with the right physic settings
+     */
     Player.prototype.init = function() {
         this.injector.physics.arcade.enable(this);
         this.body.bounce.y = root.settings.game.players.bounce.y;
         this.body.gravity.y = root.settings.game.players.gravity.y;
         this.body.collideWorldBounds = true;
+        this.body.linearDamping = 1;
     };
 
+    /**
+     * Set collision bounds for the player with a target
+     * @param  {*} target               Target to collide with
+     * @param  {Function} die           Die handler
+     */
     Player.prototype.collide = function(target, die) {
         this.injector.game.arcade.collide(this, target, die, null);
     };
 
+    /**
+     * Constantly run with the velocity set in the game settings of the players
+     */
     Player.prototype.run = function() {
         this.body.velocity.x = root.settings.game.players.velocity.x;
     };
 
+    /**
+     * Let the player jump
+     */
     Player.prototype.jump = function() {
         if(this.body.touching.down) {
             this.body.velocity.y = root.settings.game.players.velocity.y;
@@ -206,8 +228,123 @@
 (function(window, undefined) {
     'use strict';
 
+    var id = 0;
+
+    /**
+     * Constructor for a new procedure
+     * @param {String} name             Name of the procedure
+     * @param {Object} opts             Optional options
+     * @param {Function} procedure      Main procedure method
+     */
+    function Procedure(name, opts, procedure) {
+        if(typeof opts === 'function') {
+            this.options = {};
+            this.procedure = opts;
+        } else {
+            this.options = opts;
+            this.procedure = procedure;
+        }
+        this.$id = 0;
+        this.name = name;
+
+        return Container.procedures[this.name] = this;
+    };
+
+    Procedure.prototype = {
+        /**
+         * Returns the main procedure
+         * @return {Function}       Procedure
+         */
+        getFunction: function() {
+            return this.procedure;
+        },
+        /**
+         * Get the name of the procedure
+         * @return {String}         Name
+         */
+        getName: function() {
+            return this.name;
+        },
+        /**
+         * Run the procedure with arguments and callback
+         * @param  {*} input                Data to pass
+         * @param  {Function} finished      Callback handler
+         */
+        run: function(input, finished) {
+            finished = Util.default(finished, Util.noop);
+            try {
+                var result = this.procedure(input);
+                finished(result);
+            } catch(failed) {
+                finished(failed);
+            }
+        }
+    };
+
+    window.Factory.Procedure = Procedure;
+
+})(window);
+
+(function(window, undefined) {
+    'use strict';
+
+    var root = window.Container;
+    var util = window.Util;
+    var id = 0;
+
+    /**
+     * Sprite constructor for creating Spritesheets for background
+     * images or player sprites.
+     * @namespace Factory
+     * @param {Game} game           Game injector point
+     * @param {String} image        Name of the (loaded) asset
+     * @param {String} path         Path to the asset image (optional)
+     */
+    function Sprite(game, image, path) {
+        this.$id = id++;
+        this.injector = game;
+        this.image = image;
+        this.path = path;
+
+        return this;
+    };
+
+    Sprite.prototype = Object.create(Phaser.Sprite.prototype);
+    Sprite.prototype.constructor = Sprite;
+
+    /**
+     * If the asset hasn't been loaded in the boot state you
+     * can load the image with this method.
+     * @param  {String} path        Optional path (if not set in ctor)
+     * @return {Sprite} this        Return the object itself for chaning
+     */
+    Sprite.prototype.load = function(path) {
+        path = util.default(path, this.path);
+        this.injector.load.image(this.image, path);
+        return this;
+    };
+
+    Sprite.prototype.add = function(x, y) {
+        x = util.default(x, 0);
+        y = util.default(y, 0);
+        this.injector.add.sprite(x, y, this.image);
+        return this;
+    };
+
+    window.Factory.Sprite = Sprite;
+
+})(window);
+
+(function(window, undefined) {
+    'use strict';
+
     var root = window.Container;
 
+    /**
+     * Constructor of the Tilemap Class
+     * @param {Game} game           Game injector point
+     * @param {String} name         Name of the tilemap
+     */
     function Tilemap(game, name) {
         this.name = name;
         this.injector = game;
@@ -219,30 +356,59 @@
     Tilemap.prototype = Object.create(Phaser.Tilemap.prototype);
     Tilemap.prototype.constructor = Tilemap;
 
+    /**
+     * Adds a new tileset image to the tilemap. Needed is
+     * the tileset itself and the tile asset.
+     * @param  {String} tileset         Name of the tileset
+     * @param  {String} asset           Asset of the tileset
+     */
     Tilemap.prototype.addImage = function(tileset, asset) {
         return this.map.addTilesetImage(tileset, asset);
     };
 
+    /**
+     * Add the tilemap to the game, the constructor game
+     * can be used, alternatively you can pass a new game param
+     * to this method.
+     * @param  {Game} game          Phaser game
+     * @return {Tilemap}            The tilemap itself
+     */
     Tilemap.prototype.addToGame = function(game) {
         game = game || this.game || null;
         return this.map = game.add.tilemap(this.name);
     };
 
+    /**
+     * Create a new layer defined in the tilemap itself.
+     * @param  {String} name        Name of the layer
+     * @return {TilemapLayer}       Phaser tilemap layer
+     */
     Tilemap.prototype.createLayer = function(name) {
         var layer = this.map.createLayer(name);
         return this.layers[name] = layer;
     };
 
+    /**
+     * Set collision bounds between start and endpoint on
+     * a specific layer. Needed for the players to collide
+     * with the tilemap itself!
+     * @param  {String} layer       Name of the layer
+     * @param  {Number} start       Startpoint of collision
+     * @param  {Number} end         Endpoint of collision
+     */
     Tilemap.prototype.setCollision = function(layer, start, end) {
         try {
             var collide = this.map.setCollisionBetween(start, end, true, layer);
-            console.log(collide)
             return collide;
         } catch(noCollide) {
             throw new Error('Tilemap.setCollision faield: ' + noCollide.message);
         }
     };
 
+    /**
+     * Resize the world with the size of the target layer.
+     * @param  {String} targetLayer     Layer to get size from
+     */
     Tilemap.prototype.resize = function(targetLayer) {
         try {
             this.layers[targetLayer].resizeWorld();
@@ -265,7 +431,8 @@
     var loader = {
         images: {
             player: 'assets/img/avatars/player.png',
-            demoTile: 'assets/img/world/tiles/demo.png'
+            demoTile: 'assets/img/world/tiles/demo.png',
+            sky: 'assets/img/sky.png'
         },
         sprites: {
             playerExample: {
@@ -314,6 +481,7 @@
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
             this.scale.pageAlignHorizontally = true;
             if (!this.game.device.desktop) {
+                // Adding mobile support (phaser example)
                 this.scale.minWidth = 250;
                 this.scale.minHeight = 250;
                 this.scale.maxWidth = 600;
@@ -358,21 +526,11 @@
         create: function() {
             var self = this;
 
+            // Adding background image
+            this.$createBackground();
+
             // Instanciate the tilemap
             this.$createTilemap();
-
-            window.Container.Map = new Factory.Map({
-                tilemap: {
-                    name: 'demoTilemap',
-                    tiles: {
-                        name: 'demo',
-                        id: 'demoTile'
-                    },
-                    layer: {
-                        name: 'layer1'
-                    }
-                }
-            });
 
             // Create players set in settings file under /app
             this.$createPlayers(function() {
@@ -383,7 +541,7 @@
         update: function() {
             var self = this;
             Container.World.players.forEach(function(player) {
-                Container.game.physics.arcade.collide(player, Container.World.tilemap);
+                Container.game.physics.arcade.collide(player, Container.World.tilemapLayer);
                 player.$update();
                 player.run();
             });
@@ -391,12 +549,28 @@
         render: function() {
             this.game.debug.text('FPS ' + (this.game.time.fps || '--'), 20, 70, "#00ff00", "20px Courier");
         },
+        $createBackground: function() {
+            var background = new Factory.Sprite(this, 'sky');
+            background.add(0, 0);
+        },
         $createTilemap: function() {
-            var map = new Factory.Tilemap(this, 'demoTilemap');
-            map.addToGame(this);
-            map.addImage('demo', 'demoTile');
-            map.createLayer('layer1'); // Works until here
-            map.resize('layer1');
+            var self = this;
+            Container.procedures.addTilemap.run({
+                game: self,
+                tilemap: 'demoTilemap',
+                layer: {
+                    name: 'layer1',
+                    start: 0,
+                    end: 1000
+                },
+                tile: {
+                    name: 'demo',
+                    asset: 'demoTile'
+                }
+            }, function(result) {
+                Container.World.tilemap = result.tilemap.map;
+                Container.World.tilemapLayer = result.layer;
+            });
         },
         $createPlayers: function(callback) {
             var self = this;
@@ -434,7 +608,7 @@
         },
         create: function() {
             if(this.ready) {
-                this.state.start('Game');
+                this.state.start('Procedures');
             } else {
                 throw this.error;
             }
@@ -442,6 +616,42 @@
         quit: function(pointer) {
             alert('Goodbye');
             this.ready = false;
+        }
+    };
+
+})(window);
+
+(function(window, undefined) {
+    'use strict';
+
+    Container.Procedures = function(game) {
+        // Empty class wrapper
+    };
+
+    Container.Procedures.prototype = {
+        preload: function() {
+            Container.procedures = {};
+        },
+        create: function() {
+            /**
+             * Adding a tilemap to the game procedure
+             * @procedure addTilemap
+             */
+            new Factory.Procedure('addTilemap', function(data) {
+                var map = new Factory.Tilemap(data.game, data.tilemap);
+                map.addToGame(data.game);
+                map.addImage(data.tile.name, data.tile.asset);
+                var layer = map.createLayer(data.layer.name);
+                map.setCollision(data.layer.name, data.layer.start, data.layer.end);
+                map.resize(data.layer.name);
+
+                return {
+                    tilemap: map,
+                    layer: layer
+                };
+            });
+
+            this.state.start('Game');
         }
     };
 
@@ -517,6 +727,7 @@
     game.state.add('Boot', Container.Boot);
     game.state.add('Preload', Container.Preload);
     game.state.add('Game', Container.Game);
+    game.state.add('Procedures', Container.Procedures);
     game.state.start('Boot'); //starting the boot state
 
     Container.game = game;
