0a179e945eb2adf93bd4e4733f8903dc9435881c
Jan Biasi <jan.biasi@namics.com>
2016-02-01 17:37:35 +0100

    Update main files

--- app/main.js
+++ app/main.js
@@ -3,15 +3,34 @@
 
     var config = Container.settings.render;
 
-    var game = new Phaser.Game(config.width, config.height, config.mode, config.node);
+    var fade = function(element) {
+        var op = 1;  // initial opacity
+        var timer = setInterval(function () {
+            if (op <= 0.1){
+                clearInterval(timer);
+                element.style.display = 'none';
+            }
+            element.style.opacity = op;
+            element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+            op -= op * 0.1;
+        }, 50);
+    };
 
-    //adding all the required states
-    game.state.add('Boot', Container.Boot);
-    game.state.add('Preload', Container.Preload);
-    game.state.add('Game', Container.Game);
-    game.state.add('Procedures', Container.Procedures);
-    game.state.start('Boot'); //starting the boot state
+    document.getElementById('js-start-game').addEventListener('click', function() {
+        fade(document.getElementById('js-hide-start'));
+        Container.settings.worldType = document.getElementById('js-world').value;
 
-    Container.game = game;
+
+        var game = new Phaser.Game(config.width, config.height, config.mode, config.node);
+
+        //adding all the required states
+        game.state.add('Boot', Container.Boot);
+        game.state.add('Preload', Container.Preload);
+        game.state.add('Game', Container.Game);
+        game.state.add('Procedures', Container.Procedures);
+        game.state.start('Boot'); //starting the boot state
+
+        Container.game = game;
+    });
 
 })(window);
--- app/provider.js
+++
@@ -1,9 +0,0 @@
-(function(window, undefined) {
-    'use strict';
-
-    window.Util = {};
-    window.Game = {};
-    window.Container = {};
-    window.Factory = {};
-
-})(window);
