13925f9fd16c4a15b125ca7602c887b4c4b42e95
Jan Biasi <jan.biasi@namics.com>
2016-02-05 10:51:14 +0100

    Update loading performance

--- app/main.js
+++ app/main.js
@@ -7,9 +7,8 @@
 
     var settings = Container.settings;
     var config = settings.render;
-    var game = null;
 
-    var pregame = new HUD.Factory.Stepper($('.pregame.steps'));
+    var pregame = Container.stepper = new HUD.Factory.Stepper($('.pregame.steps'));
     pregame.start(function($lastStep) {
 
         // Get the current selected world and players
@@ -18,7 +17,7 @@
         settings.game.players.amount = settings.currentPlayers.length;
 
         // Create a new phaser game
-        game = new Phaser.Game(config.width, config.height, config.mode, config.node);
+        var game = new Phaser.Game(config.width, config.height, config.mode, config.node);
 
         // Adding all required phaser-game-states
         game.state.add('Boot', Container.Boot);
@@ -26,24 +25,24 @@
         game.state.add('Game', Container.Game);
         game.state.add('Procedures', Container.Procedures);
 
+        // Show the last info container on end
+        game.finishedCallback = function() {
+            var content = $('.js-finished').html();
+            var playerScores = Util.getPlayerScoreData();
 
-                // Show the last info container on end
-                game.finishedCallback = function() {
-                    var content = $('.js-finished').html();
-                    var playerScores = Util.getPlayerScoreData();
-
-                    $('.js-finished').html(Util.replace(content, playerScores));
-                    $('#' + config.node).fadeOut(function() {
-                        $('.js-finished').fadeIn();
-                    });
-                };
+            $('.js-finished').html(Util.replace(content, playerScores));
+            $('#' + config.node).fadeOut(function() {
+                $('.js-finished').fadeIn();
+            });
+        };
 
         // Make game accessable
         Container.game = game;
 
         // Fade out the last step and start the game
-        $lastStep.fadeOut(1500, function() {
-            $('.steps').remove(); // remove the stepper container and HTML nodes
+        $lastStep.slideUp(1500, function() {
+            $('.steps').remove();
+            // remove the stepper container and HTML nodes
             game.state.start('Boot'); // starting the boot state
         });
     });
