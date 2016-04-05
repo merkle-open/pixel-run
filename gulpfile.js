49c8a039cc739d6e44efc516abdf85191d221e8b
Jan Biasi <jan.biasi@namics.com>
2016-04-05 13:19:41 +0200

    Fix delete error on dist files

--- gulpfile.js
+++ gulpfile.js
@@ -37,10 +37,6 @@ gulp.task('clean:dist', function() {
     return remove(config.clean.dist);
 });
 
-gulp.task('clean:dist:app', function() {
-    return remove(config.app.target + '/' + config.app.name + '.*');
-});
-
 gulp.task('clean:dist:bundle', function() {
     return remove(config.bundle.target + '/' + config.bundle.name + '*.js');
 });
@@ -83,7 +79,7 @@ gulp.task('build:dependencies', ['clean:dist:dependencies'], function() {
         .pipe(gulp.dest(config.dependencies.target));
 });
 
-gulp.task('build:app', ['clean:dist:app'], function() {
+gulp.task('build:app', ['clean:dist:bundle'], function() {
     return gulp.src(config.app.files)
         .pipe(gulpif(config.app.minify === true, uglify()))
         .pipe(concat(config.app.name + '.js'))
