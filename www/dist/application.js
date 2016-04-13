fe8108eb4f7fe0ffe5723c1fb5ab26f91b51fd7f
Jan Biasi <jan.biasi@namics.com>
2016-04-13 10:17:01 +0200

    Fix scrollbars on have fun

--- www/dist/application.js
+++ www/dist/application.js
@@ -1,5 +1,5 @@
 /**
- * Pixel. Run. Namics. (Build HkS2rOs)
+ * Pixel. Run. Namics. (Build BkGv1Kj)
  * @author Jan Biasi <jan.biasi@namics.com>
  * @version v1.5.0-alpha
  * @license MIT Licensed by Namics AG
