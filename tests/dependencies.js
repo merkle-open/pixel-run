840caa5aead4c4b1aa83f1bd5caad73bad19630c
Jan Biasi <jan.biasi@namics.com>
2016-04-14 11:08:15 +0200

    Upgrade tests for dependencies

--- tests/dependencies.js
+++ tests/dependencies.js
@@ -1,7 +1,8 @@
 var assert = require('chai').assert;
 var bower = require('bower-json');
+var npm = require('package-json-validator').PJV;
 
-describe('Pixel-Run Game', function() {
+describe('Dependencies', function() {
     describe('bower.json', function() {
         it('should be a valid bower file', function() {
             bower.read('./../bower.json', function (err, json) {
@@ -9,6 +10,14 @@ describe('Pixel-Run Game', function() {
                 assert.isUndefined(err);
                 assert.isDefined(json);
             });
+        });
+    });
+
+    // TODO: fix this test procedure
+    describe('package.json', function() {
+        it('should be a valid npm file', function() {
+            var res = npm.validate(require('../package.json'), 'npm');
+            assert.isTrue(res.valid, 'the file is valid');
         });
     });
 });
