0e3912669eb3ce35278047b34d37f1af6e15f80f
Oliver Eisenhut <oliver.eisenhut@namics.com>
2018-01-03 17:28:40 +0100

    Remove example redux actions

--- client/GameApp/redux/actions.js
+++ client/GameApp/redux/actions.js
@@ -2,9 +2,6 @@
  * action types
  */
 
-export const ADD_TODO = "ADD_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
-export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
 export const ADD_PLAYERS = "ADD_PLAYERS";
 export const ADD_WORLD = "ADD_WORLD";
 export const ADD_INDICATE_FUNCTION = "ADD_INDICATE_FUNCTION";
@@ -28,18 +25,6 @@ export const TOGGLE_GAME_STATE = "TOGGLE_GAME_STATE";
 /*
  * action creators
  */
-
-export function addTodo(text) {
-  return { type: ADD_TODO, text };
-}
-
-export function toggleTodo(index) {
-  return { type: TOGGLE_TODO, index };
-}
-
-export function setVisibilityFilter(filter) {
-  return { type: SET_VISIBILITY_FILTER, filter };
-}
 
 export function addPlayers(players) {
   return { type: ADD_PLAYERS, players };
